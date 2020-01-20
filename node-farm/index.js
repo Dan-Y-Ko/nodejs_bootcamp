f6ddb771f9db3eb6e79448c0df68e97ac3c810f0
Daniel Ko <danko56666@gmail.com>
2020-01-19 21:52:47 -0500

    simple api

--- node-farm/index.js
+++ node-farm/index.js
@@ -32,6 +32,9 @@ console.log("File written!"); */
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Server
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -39,6 +42,12 @@ const server = http.createServer((req, res) => {
     res.end("this is the overview");
   } else if (pathName === "/product") {
     res.end("this is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json"
+    });
+
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html"
