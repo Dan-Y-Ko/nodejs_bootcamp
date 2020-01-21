1cce645459066f76ecd900ca81078bfda38e429e
Daniel Ko <danko56666@gmail.com>
2020-01-21 16:24:09 -0500

    setup basic route

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('hello from the server');
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
