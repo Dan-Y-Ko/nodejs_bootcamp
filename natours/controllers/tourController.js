1bc6dfafeb564eaf8370e6b02ba8918bb0c72fe8
Daniel Ko <danko56666@gmail.com>
2020-01-22 17:20:58 -0500

    add create tour route with mongodb

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('../models/tourModel');
 
-const tours = JSON.parse(
+/* const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+); */
 
-exports.checkId = (req, res, next, val) => {
+/* exports.checkId = (req, res, next, val) => {
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -13,9 +14,9 @@ exports.checkId = (req, res, next, val) => {
   }
 
   next();
-};
+}; */
 
-exports.checkBody = (req, res, next) => {
+/* exports.checkBody = (req, res, next) => {
   const { name, price } = req.body;
 
   if (!name || !price) {
@@ -26,7 +27,7 @@ exports.checkBody = (req, res, next) => {
   }
 
   next();
-};
+}; */
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -50,23 +51,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
