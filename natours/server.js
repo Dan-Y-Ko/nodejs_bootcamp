5b60a289b56845c889a6b9ee0c46c47410d42578
Daniel Ko <danko56666@gmail.com>
2020-01-21 19:57:02 -0500

    setup environment variables and env file

--- natours/server.js
+++ natours/server.js
@@ -1,5 +1,12 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config.env'
+});
+
 const app = require('./app');
 
+// const PORT = process.env.PORT || 5000;
 const PORT = 5000;
 
 app.listen(PORT, () => {
