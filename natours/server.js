1a75aeb82bc85532046cee0a17b7a70f63f63210
Daniel Ko <danko56666@gmail.com>
2020-01-30 17:33:27 -0500

    handle unhandled rejections

--- natours/server.js
+++ natours/server.js
@@ -27,3 +27,10 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
