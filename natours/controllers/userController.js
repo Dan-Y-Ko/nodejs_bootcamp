4561cd4caac633d7a289af1d1dc7fb5b9f5cf269
Daniel Ko <danko56666@gmail.com>
2020-02-02 21:25:08 -0500

    implement delete me method

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -50,7 +50,14 @@ exports.updateMe = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.deleteMe = asyncHandler(async (req, res, next) => {});
+exports.deleteMe = asyncHandler(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
