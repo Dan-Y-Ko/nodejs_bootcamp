59adb3619710e637ed0c579498966303ffeaf547
Daniel Ko <danko56666@gmail.com>
2020-02-02 21:06:59 -0500

    update user document

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -35,6 +35,19 @@ exports.updateMe = (req, res, next) => {
 
   // 2) Filtered out unwanted fields names that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
 };
 
 exports.getUser = (req, res) => {
