0d19b443ae17a0a9a0e7bf80f75f2dbc5493ae6f
Daniel Ko <danko56666@gmail.com>
2020-02-02 21:57:50 -0500

    implement rate limiting

--- natours/app.js
+++ natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,15 +9,23 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// global middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  window: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
 
 app.use(express.json());
 
 /* Serving static files in express
 app.use(express.static(`${__dirname}/public`)); */
 
+app.use('/api', limiter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
