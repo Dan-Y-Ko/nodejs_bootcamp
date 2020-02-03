10dca12ed97a3e7021b6c0dac2e698da9f024f6d
Daniel Ko <danko56666@gmail.com>
2020-02-02 22:07:47 -0500

    implement data sanitization against nosql injection

--- natours/app.js
+++ natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +35,11 @@ app.use(
     limit: '10kb'
   })
 );
+
+// data sanitization against nosql query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
 
 /* Serving static files in express
 app.use(express.static(`${__dirname}/public`)); */
