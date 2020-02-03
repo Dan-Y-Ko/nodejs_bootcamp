5840c23f85d495b3f2f39055d487a3bd606d2b82
Daniel Ko <danko56666@gmail.com>
2020-02-02 22:02:18 -0500

    set security headers with helmet

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,17 +11,28 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // global middlewares
+
+// set security http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// rate limiter
 const limiter = rateLimit({
   max: 100,
   window: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!'
 });
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb'
+  })
+);
 
 /* Serving static files in express
 app.use(express.static(`${__dirname}/public`)); */
