a15d0fd90b8fa7e8c32167b2805282112ff9a926
Daniel Ko <danko56666@gmail.com>
2020-02-02 21:03:09 -0500

    add filterObj function

--- natours/controllers/userController.js
+++ natours/controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/userModel');
 const asyncHandler = require('../utils/asyncHandler');
 const AppError = require('../utils/AppError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = asyncHandler(async (req, res, next) => {
   const users = await User.find();
 
@@ -14,7 +22,17 @@ exports.getAllUsers = asyncHandler(async (req, res, next) => {
   });
 });
 
-exports.updateMe = (req, res, next) => {};
+exports.updateMe = (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+};
 
 exports.getUser = (req, res) => {
   res.status(500).json({
