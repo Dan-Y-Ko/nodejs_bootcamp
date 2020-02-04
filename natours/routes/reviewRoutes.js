c4aabf8e285af53fd91dcb49153cc6b068619286
Daniel Ko <danko56666@gmail.com>
2020-02-03 22:44:47 -0500

    add get all reviews and create review to review route router

--- natours/routes/reviewRoutes.js
+++ natours/routes/reviewRoutes.js
@@ -4,4 +4,9 @@ const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(reviewControlller.getAllReviews)
+  .post(authController.restrictTo('user'), reviewController.createReview);
+
 module.exports = router;
