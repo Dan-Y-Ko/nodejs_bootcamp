1d6b4934a05e9b9e72a30431b0d9c71f56edf255
Daniel Ko <danko56666@gmail.com>
2020-02-05 00:02:23 -0500

    prevent users from being able to submit multiple reviews

--- natours/models/reviewModel.js
+++ natours/models/reviewModel.js
@@ -33,6 +33,9 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Prevent user from submitting more than one review per Tour
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next) {
   // this.populate({
   //   path: 'tour',
