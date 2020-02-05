c10a38c8afd5792118674db918b0c631b252e1db
Daniel Ko <danko56666@gmail.com>
2020-02-04 22:43:55 -0500

    update data seeding script

--- natours/dev-data/data/import-dev-data.js
+++ natours/dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({
   path: './config.env'
@@ -24,11 +26,17 @@ mongoose
 
 // read json file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // import data into db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfuly loaded');
   } catch (err) {
     console.log(err);
@@ -41,6 +49,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfuly deleted');
   } catch (err) {
     console.log(err);
