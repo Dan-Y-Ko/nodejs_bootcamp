f825cbad1bfa4c0aacd7a61d83300d87ddfb4bc8
Daniel Ko <danko56666@gmail.com>
2020-02-12 16:10:54 -0500

    add booking model

--- natours-ssr/public/js/stripe.js
+++ natours-ssr/public/js/stripe.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe('pk_test_KApgLiiCmR3QfrteH7RuFoJA00KPwNb4ea');
 
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
     );
     console.log(session);
 
-    // 2) Create checkout form + chanre credit card
+    // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id
     });
