438fb127e2458f4fcbb4a84299b4317cd210fbeb
Daniel Ko <danko56666@gmail.com>
2020-02-12 16:22:22 -0500

    create new booking on successful checkout

--- natours-ssr/controllers/bookingController.js
+++ natours-ssr/controllers/bookingController.js
@@ -37,7 +37,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-/* exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
   const { tour, user, price } = req.query;
 
@@ -47,7 +47,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   res.redirect(req.originalUrl.split('?')[0]);
 });
 
-exports.createBooking = factory.createOne(Booking);
+/* exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
 exports.updateBooking = factory.updateOne(Booking);
